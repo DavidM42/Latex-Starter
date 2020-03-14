c92b93b05615f2b64ab3406ed5e3734ea493e932
DavidM42 <davidmerz@gmx.de>
2020-03-14 01:18:32 +0100

    install extension script added

---
+++ installExtensions.sh
@@ -0,0 +1,7 @@
+code --install-extension James-Yu.latex-workshop
+code --install-extension valentjn.vscode-ltex
+# plus install language pack for your language
+
+# below are german and english extensions
+# code --install-extension valentjn.vscode-ltex-en
+# code --install-extension valentjn.vscode-ltex-de
